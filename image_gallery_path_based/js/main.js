615e2086c3f5f3e898f40cbd3397ca964cbcadf4
vincentole <63076449+vincentole@users.noreply.github.com>
2021-11-07 21:55:12 +0100

    added img gallery

---
+++ image_gallery_path_based/js/main.js
@@ -0,0 +1,24 @@
+const mainImage = document.querySelector(".main-image img");
+const imageBar = document.querySelector(".image-bar");
+const slider = document.querySelector(".slider");
+
+let imgNumber = 8;
+
+// Gallery
+for (let i = 1; i <= imgNumber; i++) {
+    const newDiv = document.createElement("div");
+    const newImage = document.createElement("img");
+    let path = `../img/gallery_${i}.jpg`;
+
+    newImage.src = path;
+
+    newDiv.appendChild(newImage);
+    imageBar.appendChild(newDiv);
+    
+    newImage.onclick = e => {
+        mainImage.src = e.target.src;
+        slider.style.transform = `translateX(${(i-1) * e.target.width}px)`;
+    }
+}
+// Set Slider Width
+slider.style.width = imageBar.scrollWidth / imgNumber + "px";
