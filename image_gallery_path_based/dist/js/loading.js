ca763b92fa99c7d5759e95beda94cd35e1670881
vincentole <63076449+vincentole@users.noreply.github.com>
2021-11-07 23:24:58 +0100

    fixed loading

---
+++ image_gallery_path_based/dist/js/loading.js
@@ -0,0 +1,13 @@
+
+const script = document.getElementById("gallery-script");
+const body = document.querySelector("body");
+
+body.style.visibility = "hidden";
+
+
+script.onload = () => { 
+    body.style.visibility = "visible";
+}
+  
+
+
